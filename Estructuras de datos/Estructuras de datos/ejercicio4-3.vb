18bec5197744a81627c68b7ebf2d59a25b1e15b3
leo <leomb_punk@hotmail.com>
2017-06-05 02:24:17 -0300

    new 4.3

--- Estructuras de datos/Estructuras de datos/ejercicio4-3.vb
+++ Estructuras de datos/Estructuras de datos/ejercicio4-3.vb
@@ -17,6 +17,83 @@
     ' uno que escriba si el alumno aprobó o no
     ' uno que devuelva la lista de mejores alumnos
     Sub main()
-
+        Dim cant_alum, cant_notas As Short
+        Dim acumulador_notas, mej_prome As Double
+        pido_cantalum_cantnotas(cant_alum, cant_notas)
+        Dim alumnos(cant_alum - 1) As String
+        Dim notas_alum(cant_alum - 1, cant_notas - 1) As Short
+        Dim promedio(cant_alum - 1) As Double
+        For x = 0 To cant_alum - 1
+            Dim alu As String
+            Dim nota As Short
+            Do
+                Console.Write("Nombre: ")
+                alu = Console.ReadLine()
+            Loop Until Not nombre_vacio(alu) Xor nombre_repe(alu, alumnos)
+            alumnos(x) = alu
+            For y = 0 To cant_notas - 1
+                Do
+                    Console.Write("Nota {0}: ", y + 1)
+                    nota = Console.ReadLine()
+                Loop Until valido_rango(nota, 0, 10)
+                notas_alum(x, y) = nota
+                acumulador_notas += nota
+            Next
+            promedio(x) = calculo_promedio(acumulador_notas, cant_notas)
+            acumulador_notas = 0
+        Next
+        'listo los alumnos, sus promedios y si aprobaron o desaprobaron
+        For x = 0 To alumnos.Count - 1
+            Console.WriteLine("Alumno: {0}  Promedio: {1}, {2}", alumnos(x), promedio(x), condicion_aprobado(promedio(x), 6.0))
+        Next
+        mej_prome = mejor_promedio(promedio)
+        listo_mejores_alumnos(alumnos, promedio, mej_prome)
+        Console.ReadKey()
     End Sub
+    Private Sub pido_cantalum_cantnotas(ByRef alum As Short, ByRef nota As Short)
+        Do
+            Console.Write("Cantidad alumnos(max 40): ")
+            alum = Console.ReadLine()
+        Loop Until valido_rango(alum, 1, 40)
+        Do
+            Console.Write("Cantidad de notas(max 4): ")
+            nota = Console.ReadLine()
+        Loop Until valido_rango(nota, 1, 4)
+    End Sub
+    Private Function valido_rango(valor As Short, desde As Short, hasta As Short) As Boolean
+        Return If(valor >= desde And valor <= hasta, True, False)
+    End Function
+    Private Function nombre_vacio(nombre As String) As Boolean
+        Return If(nombre = Nothing, True, False)
+    End Function
+    Private Function nombre_repe(nombre As String, ByRef alumno_array() As String) As Boolean
+        Return alumno_array.Contains(nombre)
+    End Function
+    Private Function calculo_promedio(ByRef value As Short, cant_notas As Short) As Double
+        Return value / cant_notas
+    End Function
+    Private Function mejor_promedio(ByRef promedio_array() As Double) As Double
+        Dim alto_prome As Double = 0
+        For Each nota In promedio_array
+            If nota > alto_prome Then
+                alto_prome = nota
+            End If
+        Next
+        Return alto_prome
+    End Function
+    Private Sub listo_mejores_alumnos(ByRef alumnos_array() As String, ByRef promedios_array() As Double, promedio As Double)
+        Console.WriteLine("***Mejor/es alumno/s***" & vbCrLf & "Promedio: {0}", promedio)
+        For x = 0 To promedios_array.Count - 1
+            If promedio = promedios_array(x) Then
+                Console.WriteLine("Alumno: {0}", alumnos_array(x))
+            End If
+        Next
+    End Sub
+    Private Function condicion_aprobado(ByRef promedio As Double, condicion_aprobar As Double) As String
+        If promedio >= condicion_aprobar Then
+            Return "Aprobo"
+        Else
+            Return "Desaprobo"
+        End If
+    End Function
 End Module
