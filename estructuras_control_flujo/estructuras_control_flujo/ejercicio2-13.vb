54343c3c2d15269914068674aeeb296133a73f3a
leo <leomb_punk@hotmail.com>
2017-06-28 13:06:09 -0300

    fff

--- estructuras_control_flujo/estructuras_control_flujo/ejercicio2-13.vb
+++ estructuras_control_flujo/estructuras_control_flujo/ejercicio2-13.vb
@@ -6,14 +6,14 @@
         Console.WriteLine("Numeros primos:")
         For x As UShort = 2 To 1000
             'Console.WriteLine("x: " & x)
-            For y As Byte = 1 To x
+            For y As UShort = 1 To x
                 If (x Mod y = 0) Then 'And (x Mod 1 = 0) Then
                     c += 1
                     'Console.WriteLine("mod: " & x Mod y)
                 End If
             Next
             If c = 2 Then
-                Console.WriteLine("wea: " & x)
+                Console.WriteLine("primin: {0}", x)
                 Console.ReadKey()
             End If
             c = 0
